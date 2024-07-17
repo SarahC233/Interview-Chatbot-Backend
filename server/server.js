34ee646de765c48fe746ce92cc704e73b33958a2
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-07-17 16:11:25 +1200

    change to vercel.json to debug deployment

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
