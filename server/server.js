99e76740ad7b0dbf4020e9f8f51a7839bbdae3d0
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-07-17 15:23:54 +1200

    env file added

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const interviewRoutes = require("./routes/interviewRoutes");
+const routes = require("./routes/routes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api", interviewRoutes);
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
